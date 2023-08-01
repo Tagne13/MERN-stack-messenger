a4e6cbd05f84a3c13de42d93515265891de1b341
El Jefe <Tagne13@gmail.com>
2023-08-01 15:26:28 -0400

    created all chat routes with corresponding functions in controllers; created authorization middleware; addec search user route

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,5 +1,5 @@
 const asyncHandler = require('express-async-handler');
-const User = require('../models/userModel');
+const { User } = require('../models/index');
 const signToken = require('../utils/auth');
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -55,6 +55,19 @@ const authUser = asyncHandler(async (req, res) => {
         res.status(400);
         throw new Error('Invalid email or password');
     }
-})
+});
 
-module.exports = { registerUser, authUser };
+const allUsers = asyncHandler(async (req, res) => {
+    const keyword = req.query.search
+      ? {
+          $or: [
+            { name: { $regex: req.query.search, $options: 'i' } },
+            { email: { $regex: req.query.search, $options: 'i'} }
+          ]
+        }
+      : {};
+    const users = (await User.find(keyword)).find({ _id: { $ne: req.user._id } });
+    res.send(users);
+});
+
+module.exports = { registerUser, authUser, allUsers };
